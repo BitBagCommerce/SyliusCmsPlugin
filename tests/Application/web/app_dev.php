249ce5728bd494f47aaf4c897873bde3d034ab5b
Kamil Kokot <kamil@kokot.me>
2017-09-18 11:01:04 +0200

    Add development environment

---
+++ tests/Application/web/app_dev.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+
+$request = Request::createFromGlobals();
+
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
