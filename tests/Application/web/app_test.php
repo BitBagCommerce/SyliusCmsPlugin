2403d5c396a80c7fc4b0beb1a64b7b0a956bcf07
shinoks <shinoks@o2.pl>
2017-09-28 14:34:52 +0200

    [REST] Adding Behat tests

--- tests/Application/web/app_test.php
+++ tests/Application/web/app_test.php
@@ -1,18 +1,13 @@
 <?php
+    use Symfony\Component\Debug\Debug;
+    use Symfony\Component\HttpFoundation\Request;
 
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Request;
+    /** @var \Composer\Autoload\ClassLoader $loader */
+    $loader = require __DIR__.'/../app/autoload.php';
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-
-Debug::enable();
-
-$kernel = new AppKernel('test', true);
-
-$request = Request::createFromGlobals();
-
-$response = $kernel->handle($request);
-$response->send();
-
-$kernel->terminate($request, $response);
+    Debug::enable();
+    $kernel = new AppKernel('dev', true);
+    $request = Request::createFromGlobals();
+    $response = $kernel->handle($request);
+    $response->send();
+    $kernel->terminate($request, $response);
