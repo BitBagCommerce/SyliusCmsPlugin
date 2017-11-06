c928b71573244d99f42d02ce77a41f1608480084
shinoks <shinoks@o2.pl>
2017-11-06 20:08:53 +0100

    [Block] Associate products with block

--- src/Entity/Block.php
+++ src/Entity/Block.php
@@ -24,6 +24,7 @@ use Sylius\Component\Resource\Model\TranslationInterface;
 class Block implements BlockInterface
 {
     use ToggleableTrait;
+    use ProductAssociationTrait;
     use TranslatableTrait {
         __construct as protected initializeTranslationsCollection;
     }
@@ -31,6 +32,7 @@ class Block implements BlockInterface
     public function __construct()
     {
         $this->initializeTranslationsCollection();
+        $this->initializeProductsCollection();
     }
 
     /**
