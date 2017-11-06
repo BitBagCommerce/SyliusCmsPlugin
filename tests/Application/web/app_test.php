a4a9953194605af00708b6f27d9bafd2738d4399
Mikołaj Król <mikolaj.krol@bitbag.pl>
2017-11-06 12:57:30 +0100

    Add front controllers

---
+++ tests/Application/web/app_test.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+Debug::enable();
+
+$kernel = new AppKernel('test', true);
+
+$request = Request::createFromGlobals();
+
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
