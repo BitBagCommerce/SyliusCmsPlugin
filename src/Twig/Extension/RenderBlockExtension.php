2b6eff5b4fecf3ad52f9624750c576d69dada59e
Mikołaj Król <mikolaj.krol@bitbag.pl>
2017-11-25 12:02:26 +0100

    bitbag_render_block backward compatibility

--- src/Twig/Extension/RenderBlockExtension.php
+++ src/Twig/Extension/RenderBlockExtension.php
@@ -56,7 +56,7 @@ final class RenderBlockExtension extends \Twig_Extension
     public function getFunctions(): array
     {
         return [
-            new \Twig_SimpleFunction('bitbag_sylius_cms_plugin_render_block', [$this, 'renderBlock'], ['needs_environment' => true, 'is_safe' => ['html']]),
+            new \Twig_SimpleFunction('bitbag_render_block', [$this, 'renderBlock'], ['needs_environment' => true, 'is_safe' => ['html']]),
         ];
     }
 
