bd88f840a6adcb433e60aec147aa0145c04b3d8f
Mikołaj Król <mikolaj.krol@bitbag.pl>
2018-07-04 22:25:59 +0200

    Prepare page importer

--- src/Importer/PageImporterInterface.php
+++ src/Importer/PageImporterInterface.php
@@ -15,12 +15,13 @@ namespace BitBag\SyliusCmsPlugin\Importer;
 interface PageImporterInterface extends ImporterInterface
 {
     const CODE_COLUMN = 'code';
+    const SECTIONS_COLUMN = 'sections';
+    const CHANNELS_COLUMN = 'channels';
+    const PRODUCTS_COLUMN = 'products';
     const SLUG_COLUMN = 'slug__locale__';
     const NAME_COLUMN = 'name__locale__';
     const IMAGE_COLUMN = 'image__locale__';
-    const SECTION_COLUMN = 'section__locale__';
     const META_KEYWORDS_COLUMN = 'meta_keywords__locale__';
     const META_DESCRIPTION_COLUMN = 'meta_description__locale__';
     const CONTENT_COLUMN = 'content__locale__';
-    const CREATED_AT_COLUMN = 'created_at';
 }
