7e236f564e1a61cbbc38b728e43dbbe248e15bde
Dmitry Balabka <dmitry.balabka@gmail.com>
2019-12-14 21:24:56 +0200

    Add SlugAwareInterface for Page entity

--- src/Entity/PageInterface.php
+++ src/Entity/PageInterface.php
@@ -14,6 +14,7 @@ namespace BitBag\SyliusCmsPlugin\Entity;
 
 use Sylius\Component\Channel\Model\ChannelsAwareInterface;
 use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Model\SlugAwareInterface;
 use Sylius\Component\Resource\Model\TimestampableInterface;
 use Sylius\Component\Resource\Model\ToggleableInterface;
 use Sylius\Component\Resource\Model\TranslatableInterface;
@@ -26,12 +27,9 @@ interface PageInterface extends
     SectionableInterface,
     TimestampableInterface,
     ChannelsAwareInterface,
-    ContentableInterface
+    ContentableInterface,
+    SlugAwareInterface
 {
-    public function getSlug(): ?string;
-
-    public function setSlug(?string $slug): void;
-
     public function getCode(): ?string;
 
     public function setCode(?string $code): void;
