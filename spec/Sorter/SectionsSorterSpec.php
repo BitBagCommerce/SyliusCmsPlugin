477ffaf5c79f08df881119a4e309d29904db92e9
Patryk Grobelny <crackgm@gmail.com>
2021-01-14 10:27:03 +0100

    Added more test cases

--- spec/Sorter/SectionsSorterSpec.php
+++ spec/Sorter/SectionsSorterSpec.php
@@ -31,7 +31,7 @@ final class SectionsSorterSpec extends ObjectBehavior
         $this->shouldHaveType(SectionsSorterInterface::class);
     }
 
-    function it_sorts_sections(
+    function it_sorts_sections_with_one_element(
         PageInterface $page,
         SectionInterface $section
     ): void {
@@ -41,6 +41,55 @@ final class SectionsSorterSpec extends ObjectBehavior
         $this->sortBySections([$page])->shouldReturn(
             [
                 "SECTION_CODE" => ['section' => $section, 0 => $page]
+            ]
+        );
+    }
+
+    function it_sorts_sections_with_more_elements(
+        PageInterface $page1,
+        PageInterface $page2,
+        PageInterface $page3,
+        SectionInterface $section1,
+        SectionInterface $section2,
+        SectionInterface $section3
+    ): void {
+        $section1->getCode()->willReturn("SECTION_1_CODE");
+        $section2->getCode()->willReturn("SECTION_2_CODE");
+        $section3->getCode()->willReturn("SECTION_3_CODE");
+
+        $page1->getSections()->willReturn(new ArrayCollection(
+            [$section1->getWrappedObject(), $section3->getWrappedObject()]
+        ));
+        $page2->getSections()->willReturn(new ArrayCollection([$section3->getWrappedObject()]));
+        $page3->getSections()->willReturn(new ArrayCollection(
+            [$section2->getWrappedObject(), $section1->getWrappedObject()]
+        ));
+
+        $this->sortBySections([$page1, $page2, $page3])->shouldReturn(
+            [
+                "SECTION_1_CODE" => ['section' => $section1, 0 => $page1, 1 => $page3],
+                "SECTION_3_CODE" => ['section' => $section3, 0 => $page1, 1 => $page2],
+                "SECTION_2_CODE" => ['section' => $section2, 0 => $page3],
+            ]
+        );
+    }
+
+    function it_sorts_sections_with_less_elements(
+        PageInterface $page1,
+        PageInterface $page2,
+        SectionInterface $section1,
+        SectionInterface $section2
+    ): void {
+        $section1->getCode()->willReturn("SECTION_1_CODE");
+        $section2->getCode()->willReturn("SECTION_2_CODE");
+
+        $page1->getSections()->willReturn(new ArrayCollection([$section1->getWrappedObject()]));
+        $page2->getSections()->willReturn(new ArrayCollection([$section2->getWrappedObject()]));
+
+        $this->sortBySections([$page1, $page2])->shouldReturn(
+            [
+                "SECTION_1_CODE" => ['section' => $section1, 0 => $page1],
+                "SECTION_2_CODE" => ['section' => $section2, 0 => $page2],
             ]
         );
     }
