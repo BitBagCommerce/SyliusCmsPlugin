a419abb82617326699410f6d858933f0efc62f52
em411 <7682404+em411@users.noreply.github.com>
2021-08-04 10:01:29 +0200

    Fix missing translations and missing file code

--- src/Importer/PageImporterInterface.php
+++ src/Importer/PageImporterInterface.php
@@ -28,15 +28,17 @@ interface PageImporterInterface extends ImporterInterface
 
     public const IMAGE_COLUMN = 'image__locale__';
 
-    public const META_KEYWORDS_COLUMN = 'meta_keywords__locale__';
+    public const IMAGE_CODE_COLUMN = 'imagecode__locale__';
 
-    public const META_DESCRIPTION_COLUMN = 'meta_description__locale__';
+    public const META_KEYWORDS_COLUMN = 'metakeywords__locale__';
+
+    public const META_DESCRIPTION_COLUMN = 'metadescription__locale__';
 
     public const CONTENT_COLUMN = 'content__locale__';
 
     public const BREADCRUMB_COLUMN = 'breadcrumb__locale__';
 
-    public const NAME_WHEN_LINKED_COLUMN = 'name_when_linked__locale__';
+    public const NAME_WHEN_LINKED_COLUMN = 'namewhenlinked__locale__';
 
-    public const DESCRIPTION_WHEN_LINKED_COLUMN = 'description_when_linked__locale__';
+    public const DESCRIPTION_WHEN_LINKED_COLUMN = 'descriptionwhenlinked__locale__';
 }
