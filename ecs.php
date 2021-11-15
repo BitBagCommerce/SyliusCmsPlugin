215d48d71919df91eeea8ecf3bd892c3b8870db6
peterXD99 <piotr.leo@bitbag.pl>
2021-11-15 23:40:03 +0100

    Edited ECS for all needed fixers

--- ecs.php
+++ ecs.php
@@ -2,17 +2,18 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use BitBag\SyliusCmsPlugin\Fixer\AboveTwoArgumentsMultilineFixer;
+use BitBag\SyliusCmsPlugin\Fixer\FinalClassInEntitiesOrRepositoriesFixer;
+use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
     $services = $containerConfigurator->services();
-    $services->set(ArraySyntaxFixer::class)
-        ->call('configure', [[
-            'syntax' => 'short',
-        ]]);
+    $services->set(AboveTwoArgumentsMultilineFixer::class);
+    $services->set(FinalClassInEntitiesOrRepositoriesFixer::class);
+    $services->set(YodaStyleFixer::class);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
