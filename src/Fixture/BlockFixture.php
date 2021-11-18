2a5fc36a6f6c6d0818b846b583127623aa0cd961
piotrleo <piotr.leo@bitbag.pl>
2021-11-18 14:40:46 +0100

    Code cleaning in BlockFixture.php

--- src/Fixture/BlockFixture.php
+++ src/Fixture/BlockFixture.php
@@ -15,6 +15,7 @@ use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\BooleanNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\IntegerNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 
 final class BlockFixture extends AbstractFixture
@@ -39,71 +40,49 @@ final class BlockFixture extends AbstractFixture
 
     protected function configureOptionsNode(ArrayNodeDefinition $optionsNode): void
     {
+        $optionsNode
+            ->children()
+                ->arrayNode('custom')
+                    ->arrayPrototype()
+                        ->children()
+                            ->booleanNode('remove_existing')->defaultTrue()->end()
+                            ->integerNode('number')->defaultNull()->end()
+                            ->booleanNode('last_four_products')->defaultFalse()->end()
+                            ->booleanNode('enabled')->defaultTrue()->end()
+                            ->integerNode('products')->defaultNull()->end()
+                            ->append($this->createArrayOfScalars('productCodes'))
+                            ->append($this->createArrayOfScalars('taxons'))
+                            ->append($this->createArrayOfScalars('sections'))
+                            ->append($this->createArrayOfScalars('channels'))
+                            ->append($this->translationsConfiguration())
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
 
+    private function createArrayOfScalars(string $name): NodeDefinition
+    {
+        $nodeDefinition = new ScalarNodeDefinition($name);
+        $nodeDefinition->defaultNull()->end();
+        return $nodeDefinition;
+    }
 
-        $removeExistingNodeDefinition = new BooleanNodeDefinition('remove_existing');
-        $removeExistingNodeDefinition->defaultTrue()->end();
-
-        $numberNodeDefinition = new IntegerNodeDefinition('number');
-        $numberNodeDefinition->defaultNull()->end();
-
-        $lastFourProductsNodeDefinition = new BooleanNodeDefinition('last_four_products');
-        $lastFourProductsNodeDefinition->defaultFalse()->end();
-
-        $enabledNodeDefinition = new BooleanNodeDefinition('enabled');
-        $enabledNodeDefinition->defaultTrue()->end();
-
-        $productsNodeDefinition = new IntegerNodeDefinition('products');
-        $productsNodeDefinition->defaultNull()->end();
-
-        $productCodesNodeDefinition = new ArrayNodeDefinition('productCodes');
-        $productCodesNodeDefinition->scalarPrototype()->end();
-
-        $taxonsNodeDefinition = new ArrayNodeDefinition('taxons');
-        $taxonsNodeDefinition->scalarPrototype()->end();
-
-        $sectionsNodeDefinition = new ArrayNodeDefinition('sections');
-        $sectionsNodeDefinition->scalarPrototype()->end();
-
-        $channelsNodeDefinition = new ArrayNodeDefinition('channels');
-        $channelsNodeDefinition->scalarPrototype()->end();
-
+    private function translationsConfiguration(): NodeDefinition
+    {
         $translationsNodeDefinition = new ArrayNodeDefinition('translations');
-        $translationsNodeArrayPrototype = $translationsNodeDefinition->arrayPrototype();
+        $translationsNodeDefinition
+            ->arrayPrototype()
+                ->children()
+                    ->scalarNode('name')->end()
+                    ->scalarNode('content')->end()
+                    ->scalarNode('link')->end()
+                    ->scalarNode('image_path')->end()
+                ->end()
+            ->end()
+        ;
 
-        $nameNodeDefinition = new ScalarNodeDefinition('name');
-        $nameNodeDefinition->defaultNull()->end();
-
-        $contentNodeDefinition = new ScalarNodeDefinition('content');
-        $contentNodeDefinition->defaultNull()->end();
-
-        $linkNodeDefinition = new ScalarNodeDefinition('link');
-        $linkNodeDefinition->defaultNull()->end();
-
-        $imagePathNodeDefinition = new ScalarNodeDefinition('image_path');
-        $imagePathNodeDefinition->defaultNull()->end();
-
-        $translationsNodeArrayPrototype
-            ->append($nameNodeDefinition)
-            ->append($contentNodeDefinition)
-            ->append($linkNodeDefinition)
-            ->append($imagePathNodeDefinition);
-
-        $customNodeDefinition = new ArrayNodeDefinition('custom');
-        $generalArrayNodeDefinition = $customNodeDefinition->arrayPrototype();
-        $generalArrayNodeDefinition
-            ->append($removeExistingNodeDefinition)
-            ->append($numberNodeDefinition)
-            ->append($lastFourProductsNodeDefinition)
-            ->append($enabledNodeDefinition)
-            ->append($productsNodeDefinition)
-            ->append($productCodesNodeDefinition)
-            ->append($taxonsNodeDefinition)
-            ->append($sectionsNodeDefinition)
-            ->append($channelsNodeDefinition)
-            ->append($translationsNodeDefinition);
-
-        $customNodeDefinition->append($generalArrayNodeDefinition);
-        $optionsNode->append($customNodeDefinition);
+        return $translationsNodeDefinition;
     }
 }
