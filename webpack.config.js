5327ff1d97b06f6b72458c7ac15739efcd41da59
Harvel218 <gacekrzysztof.218@gmail.com>
2021-11-25 12:29:49 +0100

    stable version of plugin - everything works, still can be improved

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ const pluginName = 'cms';
 const getConfig = (pluginName, type) => {
     Encore.reset();
 
-    Encore
-        .setOutputPath(`public/build/bitbag/${pluginName}/${type}/`)
+    Encore.setOutputPath(`public/build/bitbag/${pluginName}/${type}/`)
         .setPublicPath(`/build/bitbag/${pluginName}/${type}/`)
         .addEntry(`bitbag-${pluginName}-${type}`, path.resolve(__dirname, `./src/Resources/assets/${type}/entry.js`))
         .disableSingleRuntimeChunk()
@@ -18,11 +17,10 @@ const getConfig = (pluginName, type) => {
     config.name = `bitbag-${pluginName}-${type}`;
 
     return config;
-}
+};
 
-Encore
-    .setOutputPath(`src/Resources/public/`)
-    .setPublicPath(`/public/`)
+Encore.setOutputPath(`src/Resources/public/build/`)
+    .setPublicPath(`/public/build/`)
     .addEntry(`bitbag-${pluginName}-shop`, path.resolve(__dirname, `./src/Resources/assets/shop/entry.js`))
     .addEntry(`bitbag-${pluginName}-admin`, path.resolve(__dirname, `./src/Resources/assets/admin/entry.js`))
     .cleanupOutputBeforeBuild()
@@ -34,7 +32,7 @@ distConfig.name = `bitbag-plugin-dist`;
 
 Encore.reset();
 
-const shopConfig = getConfig(pluginName, 'shop')
-const adminConfig = getConfig(pluginName, 'admin')
+const shopConfig = getConfig(pluginName, 'shop');
+const adminConfig = getConfig(pluginName, 'admin');
 
 module.exports = [shopConfig, adminConfig, distConfig];
