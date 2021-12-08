cc62e53869e89c7e14629ef4464941f1be082e5a
piotrleo <93577309+piotrleo@users.noreply.github.com>
2021-12-08 15:35:29 +0100

    Validation override fix (#406)
    * Validation override fix

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusCmsPlugin\Application;
 
+use BitBag\SyliusCmsPlugin\DependencyInjection\Compiler\AuthenticationManagerPolyfillPass;
 use PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer;
 use Sylius\Bundle\CoreBundle\Application\Kernel as SyliusKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
