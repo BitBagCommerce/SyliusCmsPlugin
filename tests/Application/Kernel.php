f2fc6b406a0f20a2fa5a5e51616021358e78270e
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2022-02-16 13:27:17 +0100

    Add changes required by Sylius 1.11

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -68,7 +68,7 @@ final class Kernel extends BaseKernel
 
     protected function getContainerBaseClass(): string
     {
-        if ($this->isTestEnvironment()) {
+        if ($this->isTestEnvironment() && class_exists(MockerContainer::class)) {
             return MockerContainer::class;
         }
 
---
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+];
