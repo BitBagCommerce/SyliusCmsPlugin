54e8376aa3d50921c1f3e2edc368a891c447ce45
patryk <patryk.pogorzelski@bitbag.pl>
2022-06-27 15:41:48 +0200

    fixes ckeditor token

--- src/Form/Type/WysiwygType.php
+++ src/Form/Type/WysiwygType.php
@@ -32,7 +32,7 @@ final class WysiwygType extends AbstractType
             'config' => [
                 'filebrowserUploadUrl' => $this->urlGenerator->generate('bitbag_sylius_cms_plugin_admin_upload_editor_image'),
                 'bodyId' => 'bitbag-ckeditor',
-                'removePlugins' => 'exportpdf'
+                'removePlugins' => 'exportpdf',
             ],
         ]);
     }
