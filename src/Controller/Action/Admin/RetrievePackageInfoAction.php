1e0802f82ad2b92fcbcf5213050a276b0d29af54
Nicolas Kroll <nicolas.kroll@bitbag.io>
2023-01-30 20:03:59 +0100

    Add package management calls

---
+++ src/Controller/Action/Admin/RetrievePackageInfoAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BitBag\SyliusCmsPlugin\Controller\Action\Admin;
+
+use Symfony\Component\HttpFoundation\Response;
+
+final class RetrievePackageInfoAction
+{
+    public function __invoke(): Response
+    {
+        try {
+            file_get_contents('https://intranet.bitbag.shop/retrieve-package-info?packageName="bitbag/cms-plugin"');
+        } catch (\Exception $exception) {
+            return new Response('', Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response();
+    }
+}
