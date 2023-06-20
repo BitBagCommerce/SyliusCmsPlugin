742ceabc16b0e05ba0dd5cb93c124128634762bb
Maciej <maciej.balcewicz@bitbag.pl>
2023-06-20 13:38:13 +0200

    OP-164 - ecs revert

--- ecs.php
+++ ecs.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+    $containerConfigurator->import('vendor/bitbag/coding-standard/ecs.php');
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
