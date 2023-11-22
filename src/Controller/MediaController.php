69f50d3369ce0dd6f9925e124bffc1ac8955009e
oallain <oallain@users.noreply.github.com>
2023-11-22 18:17:54 +0100

    Fix form error

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -85,7 +85,7 @@ final class MediaController extends ResourceController
 
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $this->setMediaLocales($media, $request);
             $this->setMediaPathIfExists($media);
             $mediaTemplate = $this->mediaProviderResolver->resolveProvider($media)->getTemplate();
