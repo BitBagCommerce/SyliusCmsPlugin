74355b4c62188a4aa176ce2ee8cbcf427753e6c6
Francis Hilaire <Prometee@users.noreply.github.com>
2024-05-13 20:59:27 +0200

    ECS fix

--- tests/Application/config/bootstrap.php
+++ tests/Application/config/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '../../../vendor/autoload.php';
@@ -22,4 +24,4 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] ??= $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
-$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], \FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -7,5 +7,5 @@ use Tests\BitBag\SyliusCmsPlugin\Application\Kernel;
 require_once dirname(__DIR__) . '/../../vendor/autoload_runtime.php';
 
 return static function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
