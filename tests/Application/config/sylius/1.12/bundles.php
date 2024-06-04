f617fa8334dbe8e834bcde611acf00e79c6b6d00
Szymon Kostrubiec <szymon.kostrubiec@bitbag.pl>
2024-06-04 11:34:56 +0200

    OP-278 - Restored support for sylius 1.12

---
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+];
