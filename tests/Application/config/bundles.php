b64667b9a563543f9fb53be9a696236aae2ae8f6
Szymon Kostrubiec <szymon.kostrubiec@bitbag.pl>
2024-06-04 11:59:54 +0200

    OP-278 - Add CmsPlugin to bundles

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -61,4 +61,5 @@ return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    BitBag\SyliusCmsPlugin\BitBagSyliusCmsPlugin::class => ['all' => true],
 ];
