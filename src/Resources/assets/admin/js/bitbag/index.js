c50344d71b8b1d464c73793d3c1cb213951ec504
jkindly <kozupa.jakub@gmail.com>
2024-07-05 12:41:42 +0200

    OP-326: Single media content type + configuration

--- src/Resources/assets/admin/js/bitbag/index.js
+++ src/Resources/assets/admin/js/bitbag/index.js
@@ -1,3 +1,4 @@
+import './bitbag-content-configuration';
 export {HandleCsvUpload} from './bitbag-upload-csv';
 export {HandleSlugUpdate} from './bitbag-page-slug';
 export {HandlePreview} from './bitbag-cms-preview';
