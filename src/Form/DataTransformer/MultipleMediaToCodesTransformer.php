2903042e1d748f835813c6b0235f4541aab61c57
jkindly <kozupa.jakub@gmail.com>
2024-07-05 12:42:03 +0200

    OP-326: Multiple media content type

---
+++ src/Form/DataTransformer/MultipleMediaToCodesTransformer.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusCmsPlugin\Form\DataTransformer;
+
+use BitBag\SyliusCmsPlugin\Entity\MediaInterface;
+use BitBag\SyliusCmsPlugin\Repository\MediaRepositoryInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Form\DataTransformerInterface;
+use Webmozart\Assert\Assert;
+
+final class MultipleMediaToCodesTransformer implements DataTransformerInterface
+{
+    public function __construct(private MediaRepositoryInterface $mediaRepository)
+    {
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    public function transform($value): Collection
+    {
+        Assert::nullOrIsArray($value);
+
+        if (empty($value)) {
+            return new ArrayCollection();
+        }
+
+        return new ArrayCollection($this->mediaRepository->findBy(['code' => $value]));
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    public function reverseTransform($value): array
+    {
+        Assert::isInstanceOf($value, Collection::class);
+
+        $mediaCodes = [];
+
+        /** @var MediaInterface $media */
+        foreach ($value as $media) {
+            $mediaCodes[] = $media->getCode();
+        }
+
+        return $mediaCodes;
+    }
+}
