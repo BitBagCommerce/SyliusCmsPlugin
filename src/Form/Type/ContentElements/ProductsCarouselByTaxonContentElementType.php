85f4eecbb19f3345e4b8756312231c903972d25c
jkindly <kozupa.jakub@gmail.com>
2024-07-05 14:04:42 +0200

    OP-326: Products Carousel By Taxon content type

--- src/Form/Type/ContentElements/ProductsCarouselByTaxonContentElementType.php
+++ src/Form/Type/ContentElements/ProductsCarouselByTaxonContentElementType.php
@@ -6,36 +6,37 @@
  * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
 */
 
-
 declare(strict_types=1);
 
 namespace BitBag\SyliusCmsPlugin\Form\Type\ContentElements;
 
-use BitBag\SyliusCmsPlugin\Form\Type\MediaAutocompleteChoiceType;
 use Sylius\Bundle\ResourceBundle\Form\DataTransformer\ResourceToIdentifierTransformer;
+use Sylius\Bundle\TaxonomyBundle\Form\Type\TaxonAutocompleteChoiceType;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\ReversedTransformer;
 
-final class SingleMediaContentElementType extends AbstractType
+final class ProductsCarouselByTaxonContentElementType extends AbstractType
 {
-    public const TYPE = 'single_media';
+    public const TYPE = 'products_carousel_by_taxon';
 
-    public function __construct(private RepositoryInterface $mediaRepository)
+    public function __construct(private RepositoryInterface $taxonRepository)
     {
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add(self::TYPE, MediaAutocompleteChoiceType::class, [
-                'label' => 'bitbag_sylius_cms_plugin.ui.content_elements.type.'.self::TYPE,
+            ->add(self::TYPE, TaxonAutocompleteChoiceType::class, [
+                'label' => 'bitbag_sylius_cms_plugin.ui.taxon',
+                'choice_value' => 'code',
+                'resource' => 'sylius.taxon',
             ])
         ;
 
         $builder->get(self::TYPE)->addModelTransformer(
-            new ReversedTransformer(new ResourceToIdentifierTransformer($this->mediaRepository, 'code')),
+            new ReversedTransformer(new ResourceToIdentifierTransformer($this->taxonRepository, 'code')),
         );
     }
 
