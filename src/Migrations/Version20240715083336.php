6da89f6720420e29ed2ec9db0972c45c8c618842
jkindly <kozupa.jakub@gmail.com>
2024-07-16 08:45:07 +0200

    OP-327: Fixed column name in media channels

---
+++ src/Migrations/Version20240715083336.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusCmsPlugin\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240715083336 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels DROP FOREIGN KEY FK_D109622EE9ED820C');
+        $this->addSql('DROP INDEX IDX_D109622EE9ED820C ON bitbag_cms_media_channels');
+        $this->addSql('DROP INDEX `primary` ON bitbag_cms_media_channels');
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels CHANGE block_id media_id INT NOT NULL');
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels ADD CONSTRAINT FK_D109622EEA9FDD75 FOREIGN KEY (media_id) REFERENCES bitbag_cms_media (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_D109622EEA9FDD75 ON bitbag_cms_media_channels (media_id)');
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels ADD PRIMARY KEY (media_id, channel_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels DROP FOREIGN KEY FK_D109622EEA9FDD75');
+        $this->addSql('DROP INDEX IDX_D109622EEA9FDD75 ON bitbag_cms_media_channels');
+        $this->addSql('DROP INDEX `PRIMARY` ON bitbag_cms_media_channels');
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels CHANGE media_id block_id INT NOT NULL');
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels ADD CONSTRAINT FK_D109622EE9ED820C FOREIGN KEY (block_id) REFERENCES bitbag_cms_media (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_D109622EE9ED820C ON bitbag_cms_media_channels (block_id)');
+        $this->addSql('ALTER TABLE bitbag_cms_media_channels ADD PRIMARY KEY (block_id, channel_id)');
+    }
+}
