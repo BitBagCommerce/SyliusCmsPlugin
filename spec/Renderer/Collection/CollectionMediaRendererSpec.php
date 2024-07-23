0b53af17cd4fb961dfbe51e57defd939a67e771c
jkindly <kozupa.jakub@gmail.com>
2024-07-23 14:12:36 +0200

    OP-441: PHP Spec

---
+++ spec/Renderer/Collection/CollectionMediaRendererSpec.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace spec\BitBag\SyliusCmsPlugin\Renderer\Collection;
+
+use BitBag\SyliusCmsPlugin\Entity\CollectionInterface;
+use BitBag\SyliusCmsPlugin\Entity\MediaInterface;
+use BitBag\SyliusCmsPlugin\Renderer\Collection\CollectionMediaRenderer;
+use BitBag\SyliusCmsPlugin\Renderer\Collection\CollectionRendererInterface;
+use BitBag\SyliusCmsPlugin\Twig\Runtime\RenderMediaRuntimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use PhpSpec\ObjectBehavior;
+
+final class CollectionMediaRendererSpec extends ObjectBehavior
+{
+    function let(RenderMediaRuntimeInterface $renderMediaRuntime)
+    {
+        $this->beConstructedWith($renderMediaRuntime);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(CollectionMediaRenderer::class);
+        $this->shouldImplement(CollectionRendererInterface::class);
+    }
+
+    function it_renders_media_collection(RenderMediaRuntimeInterface $renderMediaRuntime, CollectionInterface $collection, MediaInterface $media1, MediaInterface $media2)
+    {
+        $media1->getId()->willReturn(1);
+        $media2->getId()->willReturn(2);
+
+        $media1->getCode()->willReturn('media_code_1');
+        $media2->getCode()->willReturn('media_code_2');
+
+        $collection->getMedia()->willReturn(new ArrayCollection([$media1->getWrappedObject(), $media2->getWrappedObject()]));
+
+        $renderMediaRuntime->renderMedia('media_code_1')->willReturn('media1');
+        $renderMediaRuntime->renderMedia('media_code_2')->willReturn('media2');
+
+        $this->render($collection)->shouldReturn('media1media2');
+    }
+
+    function it_renders_limited_number_of_media(RenderMediaRuntimeInterface $renderMediaRuntime, CollectionInterface $collection, MediaInterface $media1, MediaInterface $media2)
+    {
+        $media1->getId()->willReturn(1);
+        $media2->getId()->willReturn(2);
+
+        $media1->getCode()->willReturn('media_code_1');
+        $media2->getCode()->willReturn('media_code_2');
+
+        $collection->getMedia()->willReturn(new ArrayCollection([$media1->getWrappedObject(), $media2->getWrappedObject()]));
+
+        $renderMediaRuntime->renderMedia('media_code_1')->willReturn('media1');
+
+        $this->render($collection, 1)->shouldReturn('media1');
+    }
+
+    function it_supports_collections_with_media(CollectionInterface $collection, MediaInterface $media1)
+    {
+        $collection->getMedia()->willReturn(new ArrayCollection([$media1]));
+
+        $this->supports($collection)->shouldReturn(true);
+    }
+
+    function it_does_not_support_collections_without_media(CollectionInterface $collection)
+    {
+        $collection->getMedia()->willReturn(new ArrayCollection());
+
+        $this->supports($collection)->shouldReturn(false);
+    }
+}
---
+++ spec/Renderer/CollectionRendererStrategySpec.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace spec\BitBag\SyliusCmsPlugin\Renderer;
+
+use BitBag\SyliusCmsPlugin\Entity\CollectionInterface;
+use BitBag\SyliusCmsPlugin\Renderer\Collection\CollectionRendererInterface;
+use BitBag\SyliusCmsPlugin\Renderer\CollectionRendererStrategy;
+use BitBag\SyliusCmsPlugin\Renderer\CollectionRendererStrategyInterface;
+use PhpSpec\ObjectBehavior;
+
+final class CollectionRendererStrategySpec extends ObjectBehavior
+{
+    public function let(CollectionRendererInterface $renderer1, CollectionRendererInterface $renderer2): void
+    {
+        $this->beConstructedWith([$renderer1, $renderer2]);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldHaveType(CollectionRendererStrategy::class);
+    }
+
+    public function it_implements_collection_renderer_strategy_interface(): void
+    {
+        $this->shouldImplement(CollectionRendererStrategyInterface::class);
+    }
+
+    public function it_renders_collection_using_supported_renderer(
+        CollectionRendererInterface $renderer1,
+        CollectionRendererInterface $renderer2,
+        CollectionInterface $collection
+    ): void
+    {
+        $renderer1->supports($collection)->willReturn(false);
+        $renderer2->supports($collection)->willReturn(true);
+        $renderer2->render($collection, null)->willReturn('rendered content');
+
+        $this->render($collection)->shouldReturn('rendered content');
+    }
+
+    public function it_renders_collection_with_count_to_render(
+        CollectionRendererInterface $renderer1,
+        CollectionRendererInterface $renderer2,
+        CollectionInterface $collection
+    ): void
+    {
+        $renderer1->supports($collection)->willReturn(false);
+        $renderer2->supports($collection)->willReturn(true);
+        $renderer2->render($collection, 5)->willReturn('rendered content with count');
+
+        $this->render($collection, 5)->shouldReturn('rendered content with count');
+    }
+
+    public function it_returns_empty_string_when_no_renderer_supports_collection(
+        CollectionRendererInterface $renderer1,
+        CollectionRendererInterface $renderer2,
+        CollectionInterface $collection
+    ): void
+    {
+        $renderer1->supports($collection)->willReturn(false);
+        $renderer2->supports($collection)->willReturn(false);
+
+        $this->render($collection)->shouldReturn('');
+    }
+}
