cf8fb16b3ea834081b7b404efb8bd85ed57cc567
jkindly <kozupa.jakub@gmail.com>
2024-07-23 11:56:19 +0200

    OP-441: Twig function to render Collection

---
+++ src/Renderer/Collection/CollectionPagesRenderer.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusCmsPlugin\Renderer\Collection;
+
+use BitBag\SyliusCmsPlugin\Entity\CollectionInterface;
+use BitBag\SyliusCmsPlugin\Entity\PageInterface;
+use BitBag\SyliusCmsPlugin\Renderer\PageLinkRendererInterface;
+use BitBag\SyliusCmsPlugin\Sorter\SorterById;
+use Webmozart\Assert\Assert;
+
+final class CollectionPagesRenderer implements CollectionRendererInterface
+{
+    public function __construct(private PageLinkRendererInterface $pageLinkRenderer)
+    {
+    }
+
+    public function render(CollectionInterface $collection, ?int $countToRender = null): string
+    {
+        $content = '';
+        $iterator = 0;
+        Assert::notNull($collection->getPages());
+        /** @var PageInterface $page */
+        foreach (SorterById::sort($collection->getPages()->toArray(), 'desc') as $page) {
+            $content .= $this->pageLinkRenderer->render($page);
+            if (++$iterator === $countToRender) {
+                break;
+            }
+        }
+
+        return $content;
+    }
+
+    public function supports(CollectionInterface $collection): bool
+    {
+        return $collection->getPages()?->count() > 0;
+    }
+}
---
+++ src/Renderer/CollectionRendererStrategy.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusCmsPlugin\Renderer;
+
+use BitBag\SyliusCmsPlugin\Entity\CollectionInterface;
+use BitBag\SyliusCmsPlugin\Renderer\Collection\CollectionRendererInterface;
+
+final class CollectionRendererStrategy implements CollectionRendererStrategyInterface
+{
+    /**
+     * @param CollectionRendererInterface[] $renderers
+     */
+    public function __construct(private iterable $renderers)
+    {
+    }
+
+    public function render(CollectionInterface $collection, ?int $countToRender = null): string
+    {
+        foreach ($this->renderers as $renderer) {
+            if ($renderer->supports($collection)) {
+                return $renderer->render($collection, $countToRender);
+            }
+        }
+
+        return '';
+    }
+}
