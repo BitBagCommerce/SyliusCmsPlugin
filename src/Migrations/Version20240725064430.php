20047e920d14b58db0adf7b5ef795582823ab61d
jkindly <kozupa.jakub@gmail.com>
2024-07-26 08:38:43 +0200

    OP-442: Migration OS header and typo fix

--- src/Migrations/Version20240725064430.php
+++ src/Migrations/Version20240725064430.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
 declare(strict_types=1);
 
 namespace BitBag\SyliusCmsPlugin\Migrations;
@@ -11,7 +17,7 @@ final class Version20240725064430 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'This migration remove locales from pages.';
+        return 'This migration removes locales from pages.';
     }
 
     public function up(Schema $schema): void
