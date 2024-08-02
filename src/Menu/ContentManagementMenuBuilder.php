57785404dbcbf70ecb4c91ead3a7661449471647
jkindly <kozupa.jakub@gmail.com>
2024-08-02 08:53:50 +0200

    OP-463: Move CMS menu under marketing

--- src/Menu/ContentManagementMenuBuilder.php
+++ src/Menu/ContentManagementMenuBuilder.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusCmsPlugin\Menu;
 
+use Knp\Menu\ItemInterface;
 use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
 
 final class ContentManagementMenuBuilder
@@ -62,5 +63,29 @@ final class ContentManagementMenuBuilder
             ->setLabel('bitbag_sylius_cms_plugin.ui.media')
             ->setLabelAttribute('icon', 'file')
         ;
+
+        $this->reorderMenu($menu, 'bitbag_cms', 'marketing');
+    }
+
+    private function reorderMenu(ItemInterface $menu, string $newItemKey, string $targetItemKey): void
+    {
+        $menuItems = $menu->getChildren();
+
+        $newMenuItem = $menu->getChild($newItemKey);
+        unset($menuItems[$newItemKey]);
+
+        $targetPosition = array_search($targetItemKey, array_keys($menuItems), true);
+
+        if (null !== $newMenuItem && false !== $targetPosition) {
+            $menuItems = array_slice($menuItems, 0, $targetPosition + 1, true) +
+                [$newItemKey => $newMenuItem] +
+                array_slice($menuItems, $targetPosition + 1, null, true);
+
+            $menuItems = array_filter($menuItems, static function ($item) {
+                return $item instanceof ItemInterface;
+            });
+
+            $menu->setChildren($menuItems);
+        }
     }
 }
