280463a02425d5568d3b252d2b314517b74a6cfa
jkindly <kozupa.jakub@gmail.com>
2024-08-28 14:19:36 +0200

    OP-290: Doctrine query extension to filter api responses

---
+++ src/Doctrine/ORM/Extension/EnabledAndAvailableExtension.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace Sylius\CmsPlugin\Doctrine\ORM\Extension;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use Doctrine\ORM\QueryBuilder;
+use Sylius\Bundle\ApiBundle\Context\UserContextInterface;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Model\ChannelsAwareInterface;
+use Sylius\Component\Resource\Model\ToggleableInterface;
+
+final class EnabledAndAvailableExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    public function __construct(
+        private UserContextInterface $userContext,
+        private ChannelContextInterface $channelContext,
+    ) {
+    }
+
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        Operation $operation = null,
+        array $context = [],
+    ): void {
+        $this->addEnabledAndChannelCondition($queryBuilder, $queryNameGenerator, $resourceClass);
+    }
+
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        Operation $operation = null,
+        array $context = [],
+    ): void {
+        $this->addEnabledAndChannelCondition($queryBuilder, $queryNameGenerator, $resourceClass);
+    }
+
+    private function addEnabledAndChannelCondition(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass): void
+    {
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+
+        $user = $this->userContext->getUser();
+        if (null !== $user && in_array('ROLE_API_ACCESS', $user->getRoles(), true)) {
+            return;
+        }
+
+        if (in_array(ToggleableInterface::class, (array) class_implements($resourceClass), true)){
+            $queryBuilder
+                ->andWhere(sprintf('%s.enabled = :enabled', $rootAlias))
+                ->setParameter('enabled', true);
+        }
+
+        if (in_array(ChannelsAwareInterface::class, (array) class_implements($resourceClass), true)) {
+            $channelAlias = $queryNameGenerator->generateJoinAlias('channel');
+            $queryBuilder
+                ->innerJoin(sprintf('%s.channels', $rootAlias), $channelAlias)
+                ->andWhere(sprintf('%s.code = :channel', $channelAlias))
+                ->setParameter('channel', $this->channelContext->getChannel()->getCode());
+        }
+    }
+}
