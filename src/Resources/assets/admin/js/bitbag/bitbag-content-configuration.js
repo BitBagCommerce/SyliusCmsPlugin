7f5a34325bf3fa2fc9abab37cd4f111a743b10c5
jkindly <kozupa.jakub@gmail.com>
2024-08-29 11:34:20 +0200

    OP-290: Remove bitbag docblock at the top of files

--- src/Resources/assets/admin/js/bitbag/bitbag-content-configuration.js
+++ src/Resources/assets/admin/js/bitbag/bitbag-content-configuration.js
@@ -1,8 +1,4 @@
-/*
- * This file was created by developers working at BitBag
- * Do you need more information about us and what we do? Visit our https://bitbag.io website!
- * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
-*/
+
 
 $(document).ready(function() {
     $('.bitbag-media-autocomplete, .sylius-autocomplete').each((index, element) => {
